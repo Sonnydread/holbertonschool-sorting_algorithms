e7b61bc34925313c92db1d89bf1c077efb35b4ac
Sonnydread <alvaradoguille590@gmail.com>
2023-05-25 08:32:42 -0700

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,43 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - doub linked list in ascending order
+ * insertion_sort_list - doubly linked list in ascending order
  * @list: The linked list
  * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
+  if (list == NULL || *list == NULL || (*list)->next == NULL)
+    return;
 
-if (list == NULL || *list == NULL || (*list)->next == NULL)
-return;
+  listint_t *current, *key, *prev, *temp;
 
-listint_t *current = (*list)->next;
-while (current != NULL)
-{
-listint_t *key = current;
-listint_t *prev = current->prev;
+  current = (*list)->next;
+  while (current != NULL)
+    {
+      key = current;
+      prev = current->prev;
 
-while (prev != NULL && prev->n > key->n)
-{
-listint_t *temp = key->next;
+      while (prev != NULL && prev->n > key->n)
+	{
+	  temp = key->next;
 
-prev->next = key;
-key->prev = prev;
-key->next = temp;
-if (temp != NULL)
-temp->prev = key;
+	  prev->next = key;
+	  key->prev = prev;
+	  key->next = temp;
+	  if (temp != NULL)
+	    temp->prev = key;
 
-if (prev->prev != NULL)
-prev->prev->next = prev;
-else
-*list = prev;
+	  if (prev->prev != NULL)
+	    prev->prev->next = prev;
+	  else
+	    *list = prev;
 
-prev->prev = key;
-prev = prev->prev;
-print_list(*list);
-}
-current = current->next;
-}
+	  prev->prev = key;
+	  prev = prev->prev;
+	  print_list(*list);
+	}
+
+      current = current->next;
+    }
 }
