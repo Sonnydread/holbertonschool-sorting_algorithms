69aa76de4741db1e688b912a22b6bbc7b59d5635
Sonnydread <alvaradoguille590@gmail.com>
2023-05-25 08:20:40 -0700

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,25 +1,26 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - doub linked list in ascending order
- * @list: The linked list
- * Return: void
- */
+* insertion_sort_list - doubly linked list in ascending order
+* @list: The linked list
+* Return: void
+*/
 void insertion_sort_list(listint_t **list)
 {
-
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
 listint_t *current = (*list)->next;
+listint_t *key, *prev, *temp;
+
 while (current != NULL)
 {
-listint_t *key = current;
-listint_t *prev = current->prev;
+key = current;
+prev = current->prev;
 
 while (prev != NULL && prev->n > key->n)
 {
-listint_t *temp = key->next;
+temp = key->next;
 
 prev->next = key;
 key->prev = prev;
@@ -31,10 +32,12 @@ if (prev->prev != NULL)
 prev->prev->next = prev;
 else
 *list = prev;
+
 prev->prev = key;
 prev = prev->prev;
 print_list(*list);
 }
+
 current = current->next;
 }
 }
