e4acf6a355737a50a4750d1b28601684333105b2
Sonnydread <alvaradoguille590@gmail.com>
2023-05-25 13:59:26 -0700

    seleck

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,30 @@
 #include "sort.h"
 
 /**
- * selection_sort - array of integers in ascending
+ * selection_sort - array of integers in ascending order
  * @array: array of integers to be sorted
  * @size: size of the array
  * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-if (array == NULL || size <= 1)
-return;
-
 size_t R, W, min_index;
 int temp;
 
-for (R = 0; R < size - 1; R++)
+if (array == NULL || size <= 1)
+return;
+
+R = 0;
+while (R < size - 1)
 {
 min_index = R;
-for (W = R + 1; W < size; W++)
+W = R + 1;
+while (W < size)
 {
 if (array[W] < array[min_index])
 min_index = W;
+W++;
+}
 
 if (min_index != R)
 {
@@ -29,6 +33,6 @@ array[R] = array[min_index];
 array[min_index] = temp;
 print_array(array, size);
 }
-}
+R++;
 }
 }
